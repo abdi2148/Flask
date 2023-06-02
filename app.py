45e43e917319acd7500e159a8d64ebbee8ef384c
abdi2148 <abdiqafar.abas@gmail.com>
2023-06-02 10:35:50 +0200

    index.html added

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, jsonify
 from app_service import AppService
-import json
 
 app = Flask(__name__)
-appService = AppService();
-
+appService = AppService()
 
 @app.route('/')
 def home():
-    return "App Works!!!"
-
+    return render_template('index.html')
 
 @app.route('/api/tasks')
-def tasks():
-    return appService.get_tasks()
+def get_tasks():
+    tasks = appService.get_tasks()
+    return jsonify(tasks)
 
 @app.route('/api/task', methods=['POST'])
 def create_task():
-    request_data = request.get_json()
-    task = request_data['task']
-    return appService.create_task(task)
-
+    task = request.json.get('task')
+    if task:
+        new_task = appService.create_task(task)
+        return jsonify(new_task), 201
+    else:
+        return jsonify({'error': 'Invalid request'}), 400
 
 @app.route('/api/task', methods=['PUT'])
 def update_task():
-    request_data = request.get_json()
-    return appService.update_task(request_data['task'])
-
+    task = request.json.get('task')
+    if task:
+        updated_task = appService.update_task(task)
+        return jsonify(updated_task), 200
+    else:
+        return jsonify({'error': 'Invalid request'}), 400
 
 @app.route('/api/task/<int:id>', methods=['DELETE'])
 def delete_task(id):
-    return appService.delete_task(id)
+    result = appService.delete_task(id)
+    if result:
+        return jsonify({'message': 'Task deleted successfully'}), 200
+    else:
+        return jsonify({'error': 'Task not found'}), 404
 
+if __name__ == '__main__':
+    app.run()
